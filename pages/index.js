4a306c18f2ba792fc0a45249cb987a4f11e9f651
DungFly <dungfly210@gmail.com>
2023-04-17 16:02:41 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,38 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+import { loadTossPayments } from "@tosspayments/payment-sdk";
+import { useEffect } from "react";
+const clientKey = "test_ck_OyL0qZ4G1VOKj27DjXMVoWb2MQYg";
 
 export default function Home() {
+  useEffect(() => {
+    // onPay();
+  }, []);
+
+  const onPay = async () => {
+    try {
+      const tossPayments = await loadTossPayments(clientKey);
+      console.log("tossPayments: ", tossPayments);
+
+      const res = await tossPayments.requestPayment("카드", {
+        amount: 15000,
+        orderId: "yru1QbazafgqfHjk-edE5",
+        orderName: "토스 티셔츠 외 2건",
+        customerName: "박토스",
+        successUrl: "http://localhost:8081/success",
+        failUrl: "http://localhost:8081/fail",
+        flowMode: "DIRECT",
+        easyPay: "토스페이", // 페이코 / PAYCO | 카카오페이 / KAKAOPAY | 네이버페이 / NAVERPAY
+      });
+
+      console.log("res: ", res);
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +42,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <button className={styles.title} onClick={onPay}>
+          Payment
+        </button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
